70963db3532b3221bb284cdac7013212294d5e66
ParkSongSal <tkdanr0311@gmail.com>
2020-07-16 15:30:07 +0900

    Initial commit

---
+++ app/src/main/java/com/example/sudokutest/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.sudokutest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        int [][] Sudoku = SudokuGenerator.getInstance().generateGrid();
+        GameEngine.getInstance().setSudoku(Sudoku);
+        printSudoku(Sudoku);
+
+
+    }
+
+    private void printSudoku(int Sudoku[][]) {
+        for (int x = 0; x < 9; x++) {
+            for (int y = 0; y < 9; y++) {
+                //Log.d("TAG","Sudoku " + Sudoku[x][y]+"|");
+                System.out.print(Sudoku[x][y] + "|");
+            }
+            System.out.println();
+            //Log.d("TAG","Sudoku");
+        }
+    }
+}
